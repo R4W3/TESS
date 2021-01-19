1be2d3431773463f0b5dbfc3a129b78b831e2825
R4W3 <rene.ehlers199902@gmail.com>
2021-01-19 18:07:21 +0100

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -7,10 +8,28 @@ def welcome():
 
     return render_template("setup_welcome.html")
 
-@app.route("/setup")
+@app.route("/setup", methods=['GET', 'POST'])
 def setup_1():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        host = request.form['host']
+        mydb = mysql.connector.connect(
+            host=host,
+            user=username,
+            password=password,
+        )
+        if mydb:
+            return redirect('/setup2')
+        else:
+            return redirect('/setup')
 
     return render_template("setup_1.html")
 
-if __name__ == "__main__": #finally run the fucker
+@app.route("/setup2")
+def setup2():
+
+    return render_template("setup_2.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
