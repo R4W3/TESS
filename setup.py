f0f277d009ba011b457d98348b96ea272ab25708
R4W3 <rene.ehlers199902@gmail.com>
2021-01-19 17:35:31 +0100

    Initial Commit

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def welcome():
+
+    return render_template("setup_welcome.html")
+
+
 if __name__ == "__main__": #finally run the fucker
     app.run(debug=True)
